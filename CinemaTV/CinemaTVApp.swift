6562c174e244b9fc9345ab08d3fc8625f93947f8
Danilo Requena <danilo.requena26@gmail.com>
2021-11-07 10:49:24 -0300

    Initial Commit

---
+++ CinemaTV/CinemaTVApp.swift
@@ -0,0 +1,20 @@
+//
+//  CinemaTVApp.swift
+//  CinemaTV
+//
+//  Created by Danilo Requena on 07/11/21.
+//
+
+import SwiftUI
+
+@main
+struct CinemaTVApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
